69aaf60ef227b79a8f6e6d2252c82b5edfe453f6
magicalPeter <152686107+magicalPeter@users.noreply.github.com>
2025-02-18 18:06:37 +0800

    Update script.js

--- relight/js/script.js
+++ relight/js/script.js
@@ -294,6 +294,7 @@ function setupShaderAttributes() {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);  // add
 
     shaderProgram.imgSizeUnif = gl.getUniformLocation(shaderProgram, 'imgSize');
     shaderProgram.minMaxZUnif = gl.getUniformLocation(shaderProgram, 'minMaxZ');
